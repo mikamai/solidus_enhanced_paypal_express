69b2ca663658447718649a2ed3b297eaa58bd143
cesare soldini <cesare.soldini@cayenne.it>
2015-11-23 16:48:24 +0100

    Disable LiveReload on Spree admin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = false
 
-  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload, no_swf: true
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload,
+    no_swf: true,
+    ignore: [ %r{^/admin/} ]
 end
