99834a8a2bc834e604131eac436a4142ca126a0f
cesare soldini <cesare.soldini@cayenne.it>
2015-11-25 17:54:15 +0100

    Move path helpers in application helpers, or responsive menu will complain

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,4 @@
 class PagesController < ApplicationController
-  helper_method :shop_merchandising_path
-  helper_method :shop_beers_path
-  helper_method :shop_christmas_beer_path
-
   def home
     render :home
   end
@@ -13,23 +9,4 @@ class PagesController < ApplicationController
       taxons: Spree::Taxon.where(parent: parent_taxon),
     }
   end
-
-  private
-
-  def shop_merchandising_path(sub_category = nil)
-    if sub_category
-      spree.nested_taxons_path "categories/merchandising/#{sub_category.to_sym}"
-    else
-      spree.nested_taxons_path "categories/merchandising"
-    end
-  end
-
-  def shop_beers_path
-    spree.nested_taxons_path "categories/beers"
-  end
-
-  def shop_christmas_beer_path
-    spree.product_path Spree::Product.find_by_slug "birra-di-natale-forst-2l"
-  end
-
 end
---
+++ app/helpers/spree_paths_helper.rb
@@ -0,0 +1,17 @@
+module SpreePathsHelper
+  def shop_merchandising_path(sub_category = nil)
+    if sub_category
+      spree.nested_taxons_path "categories/merchandising/#{sub_category.to_sym}"
+    else
+      spree.nested_taxons_path "categories/merchandising"
+    end
+  end
+
+  def shop_beers_path
+    spree.nested_taxons_path "categories/beers"
+  end
+
+  def shop_christmas_beer_path
+    spree.product_path Spree::Product.find_by_slug "birra-di-natale-forst-2l"
+  end
+end
