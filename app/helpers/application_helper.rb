425974438016de098a1739ea313af68828d575f4
cesare soldini <cesare.soldini@cayenne.it>
2015-11-25 20:14:18 +0100

    A little better splash image logic, still TODO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def taxon_splash_image_path
+    splash = case taxon.name
+             when "Beers"         then 'catalog_beer/catalog_beer_splash.jpg'
+             when "Merchandising" then 'merchandising/merchandising.jpg'
+             when "Clothing"      then 'merchandising/merchandising.jpg'
+             when "Accessories"   then 'merchandising/merchandising.jpg'
+             when "Glasses"       then 'merchandising/merchandising.jpg'
+             end
+    image_path splash
+  end
 end
