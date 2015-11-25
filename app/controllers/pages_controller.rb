e7c8744253cc04223fbd83f8fbd8892e2376c5f6
cesare soldini <cesare.soldini@cayenne.it>
2015-11-25 20:16:44 +0100

    Add all products pages, remove relative mockup pages
     - Taxon#show page substitutes merchandising, as a taxon-with-children.
     - Products pages are Taxon#show as taxon-without-children, so show the products.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,4 @@ class PagesController < ApplicationController
   def home
     render :home
   end
-
-  def merchandising
-    parent_taxon = Spree::Taxon.find_by_name!("Merchandising")
-    render :merchandising, locals: {
-      taxons: Spree::Taxon.where(parent: parent_taxon),
-    }
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'merchandising', to: 'pages#merchandising'
   get 'mockup/:page', to: 'mockup#static'
   mount Spree::Core::Engine, at: '/'
 end
