afb87c60036a1de0ccfa1e04b847720569377100
Diego Franciosi <dfranciosi@gmail.com>
2015-11-27 15:40:08 +0100

    Add soildus_i18n

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124111028) do
+ActiveRecord::Schema.define(version: 20151126224032) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
