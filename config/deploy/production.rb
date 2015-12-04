d9515f425cc3dca4749dbfb4bfe87724f21eb8d7
Matteo Capretto <matteo.capretto@gmail.com>
2015-12-04 16:22:27 +0100

    updates prod recipe (ok I've done it directly in master, don't shoot me)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-NODES = %w(cayenne@app5.caynod.es cayenne@app6.caynod.es)
+NODES = %w(cayman.caynod.es)
 role :app, NODES
 role :web, NODES
 #role :db,  %w(cayenne@db1.caynod.es)
@@ -6,6 +6,9 @@ role :web, NODES
 set :rbenv_ruby, '2.2.3'
 set :rails_env, 'production'
 set :bundle_without, %w( development test staging ).join(' ')
+
+set :deploy_to, '/hosting/sites/forst/drunken-ibex'
+
 
 def tag_branch_target
   tags = `git tag`.split("\n")
