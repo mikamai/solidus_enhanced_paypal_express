91deabb8d7f248762ada629a838cda88c1ca38e7
Diego Franciosi <dfranciosi@gmail.com>
2015-12-04 11:16:41 +0100

    Underage page - 1st version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController < ApplicationController
   def home
     render :home
   end
+
+  def underage
+    render :underage
+  end
+
+  def yes_answer
+    redirect_to home_url
+  end
+
+  def no_answer
+    render :forbidden, status: :forbidden
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#underage'
+
+  get  'home', to: 'pages#home'
+  get  'yes-answer', to: 'pages#yes_answer'
+  get  'no-answer', to: 'pages#no_answer'
   get 'mockup/:page', to: 'mockup#static'
   mount Spree::Core::Engine, at: '/'
 end
