fa45ee220e1099a6e9f8738f9fc9d95ac6a9b073
Diego Franciosi <dfranciosi@gmail.com>
2015-12-21 09:22:15 +0100

    Privacy page rough version

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   localized do
     root to: 'pages#underage', as: :root
-    get  'home', to: 'pages#home', as: 'home'
-    get  'yes-answer', to: 'pages#yes_answer', as: 'yes_answer'
-    get  'no-answer', to: 'pages#no_answer', as: 'no_answer'
-    get  'mockup/:page', to: 'mockup#static'
-    get  'static-page', to: 'pages#static-page'
+    get  'home',          to: 'pages#home', as: 'home'
+    get  'yes-answer',    to: 'pages#yes_answer', as: 'yes_answer'
+    get  'no-answer',     to: 'pages#no_answer', as: 'no_answer'
+    get  'mockup/:page',  to: 'mockup#static'
+    get  'static-page',   to: 'pages#static-page'
+    get  'privacy',       to: 'pages#privacy', as: 'privacy'
   end
 
   mount Spree::Core::Engine, at: '/'
