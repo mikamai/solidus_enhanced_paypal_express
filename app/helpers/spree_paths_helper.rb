db5fb9eae9774eb1bbf8c1299b0fbefd0e24eb13
Diego Franciosi <dfranciosi@gmail.com>
2015-12-22 18:54:17 +0100

    Route solidus helper updates

--- app/helpers/spree_paths_helper.rb
+++ app/helpers/spree_paths_helper.rb
@@ -1,23 +1,23 @@
 module SpreePathsHelper
   def shop_merchandising_path(sub_category = nil)
     if sub_category
-      locale_path + spree.nested_taxons_path("categories/merchandising/#{sub_category.to_sym}")
+      spree.nested_taxons_path("categories/merchandising/#{sub_category.to_sym}")
     else
-      locale_path + spree.nested_taxons_path("categories/merchandising")
+      spree.nested_taxons_path("categories/merchandising")
     end
   end
 
   def shop_beers_path
-    locale_path + spree.nested_taxons_path("categories/beers")
+    spree.nested_taxons_path("categories/beers")
   end
 
   def shop_christmas_beer_path
     christmas_beer = Spree::Product.find_by_id 4
-    locale_path + spree.product_path(christmas_beer) if christmas_beer
+    spree.product_path(christmas_beer) if christmas_beer
   end
 
   def continue_shopping_path
-    locale_path + spree.products_path
+    spree.products_path
   end
 
   def locale_path
