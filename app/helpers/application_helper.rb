8da1c36b3235e017a6a8292540f090c43f0f115c
Diego Franciosi <dfranciosi@gmail.com>
2016-07-28 17:54:14 +0200

    On translations too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def taxon_splash_image_path(taxon)
     taxon_name = Globalize.with_locale(:en) { taxon.name }
-    splash = case taxon_name
-             when "Beers"         then 'catalog_beer/catalog_beer_splash.jpg'
-             when "Merchandising" then 'merchandising/merchandising.jpg'
-             when "Clothing"      then 'merchandising/merchandising.jpg'
-             when "Accessories"   then 'merchandising/merchandising.jpg'
-             when "Glasses"       then 'merchandising/merchandising.jpg'
+    splash = case taxon_name.downcase
+             when "beers"         then 'catalog_beer/catalog_beer_splash.jpg'
+             when "merchandising" then 'merchandising/merchandising.jpg'
+             when "clothing"      then 'merchandising/merchandising.jpg'
+             when "accessories"   then 'merchandising/merchandising.jpg'
+             when "glasses"       then 'merchandising/merchandising.jpg'
              end
     image_path splash
   end
