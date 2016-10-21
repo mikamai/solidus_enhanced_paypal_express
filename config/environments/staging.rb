47684457b555bd2336a8572e34c003f1261643ab
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-10-21 10:21:30 +0200

    changed env variables for staging mailer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -22,7 +22,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV.fetch('SMTP_ADDRESS'),
-    domain:               ENV.fetch('SMTP_DOMAIN'),
     user_name:            ENV.fetch('SMTP_USERNAME'),
     password:             ENV.fetch('SMTP_PASSWORD'),
     enable_starttls_auto: false,
