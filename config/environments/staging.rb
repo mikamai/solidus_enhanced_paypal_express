29c81359cd64622ef01d400d990c2b9e544598fe
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-10-21 12:44:32 +0200

    brought back domain

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
     address:              ENV.fetch('SMTP_ADDRESS'),
     user_name:            ENV.fetch('SMTP_USERNAME'),
     password:             ENV.fetch('SMTP_PASSWORD'),
+    domain:               ENV.fetch('SMTP_DOMAIN'),
     enable_starttls_auto: false,
     openssl_verify_mode:  'none',
   }
