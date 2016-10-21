32dccd4e98a4457ca99b5fdcf676b15e193e6a7f
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-10-21 12:54:18 +0200

    corrected, at last, mailer issue

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,6 +19,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV.fetch('SMTP_ADDRESS'),
