18c3339a0eb9346cad9796b601d79ed7a9026d33
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-10-25 10:44:44 +0200

    override simple weight0

---
+++ app/models/spree/calculator/shipping/simple_weight_decorator.rb
@@ -0,0 +1,23 @@
+Spree::Calculator::Shipping::SimpleWeight.class_eval do
+
+  def compute_package(package)
+    content_items = package.contents
+    line_items_total = total(content_items)
+    handling_fee = preferred_handling_max > line_items_total ? preferred_handling_fee : 0
+
+    total_weight = total_weight(content_items)
+    costs = costs_string_to_hash(clean_costs_string)
+    weight_class = costs.keys.select { |w| total_weight <= w }.min
+    additional_costs = extra_price(content_items)
+    shipping_costs = costs[weight_class] + additional_costs
+
+    shipping_costs ? shipping_costs + handling_fee : 0
+  end
+
+  private
+
+  def extra_price(content_items)
+    content_items.inject(0){ |result, item| result += item.variant.product[:packaging_cost]}
+  end
+
+end
