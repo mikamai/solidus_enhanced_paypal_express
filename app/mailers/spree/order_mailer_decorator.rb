136f42840e63d0c50f335dd7254f5e5ebe2e3d23
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-11-10 16:15:01 +0100

    removes me and giulia from mail receivers

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -4,7 +4,7 @@ Spree::OrderMailer.class_eval do
     @order = find_order(order)
     subject = build_subject(Spree.t('order_mailer.confirm_email.subject'), false)
 
-    mail(to: "giulia.valente@cayenne.it, luca.mattiazzi@cayenne.it, shop@forst.it", from: "#{@order.email}", subject: subject)
+    mail(to: "shop@forst.it", from: "#{@order.email}", subject: subject)
   end
 
 end
