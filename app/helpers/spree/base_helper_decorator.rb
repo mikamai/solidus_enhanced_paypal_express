b1ea03c7ba1b21c239c49999839355bba4778467
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-11-17 18:31:32 +0100

    added sold items

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,4 +2,13 @@ Spree::BaseHelper.module_eval do
   def has_starting_from_price?(product)
     product.price_in(Spree::Config[:currency]).starting_from
   end
+
+  def sold_items_display(product)
+    production_start = Time.new(2016, 10, 25, 12)
+    hash = product.sold_amount(production_start).each_with_object ({count: 0, amount: 0}) do |item, result|
+      result[:count]  += item.quantity
+      result[:amount] += item.quantity * item.price
+    end
+    return "#{hash[:count]} - #{hash[:amount].to_f.round(2)}€"
+  end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,6 +3,15 @@ Spree::Product.class_eval do
 
   after_create :set_price_as_starting_from
 
+  def sold_amount(from = Time.new(1988, 8, 1), to = Time.now)
+    variants_ids = self.variants_including_master.pluck(:id)
+    items_sold = Spree::LineItem.where(variant_id: variants_ids)
+                                .joins(:order)
+                                .where("spree_orders.state = ?", "complete")
+                                .where("spree_orders.completed_at >= ?", from)
+                                .where("spree_orders.completed_at <= ?", to)
+  end
+
   private
 
   def set_price_as_starting_from
