fab69c35c0c551a797b45035ce3bb4170541d97d
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-11-21 11:05:34 +0100

    no more tags for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,13 +11,13 @@ set :rbenv_ruby, '2.2.3'
 set :rails_env, 'production'
 set :bundle_without, %w( development test staging ).join(' ')
 
-def tag_branch_target
-  tags = `git tag`.split("\n")
-  sorted_tags = tags.sort {|n,m| Gem::Version.new(n) <=> Gem::Version.new(m)}
-  tag_prompt = "\nlast tags available are #{sorted_tags.last(5).reverse}"
-  ask :branch_or_tag, tag_prompt
-  tag = fetch(:branch_or_tag)
-  tag.match(/^\d/).nil? ? sorted_tags.last : tag
-end
+# def tag_branch_target
+#   tags = `git tag`.split("\n")
+#   sorted_tags = tags.sort {|n,m| Gem::Version.new(n) <=> Gem::Version.new(m)}
+#   tag_prompt = "\nlast tags available are #{sorted_tags.last(5).reverse}"
+#   ask :branch_or_tag, tag_prompt
+#   tag = fetch(:branch_or_tag)
+#   tag.match(/^\d/).nil? ? sorted_tags.last : tag
+# end
 
-set :branch, -> { tag_branch_target }
+set :branch, ENV["BRANCH"] || "master"
