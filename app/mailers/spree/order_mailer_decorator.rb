523fe4fef5a897e3a17cca5d6e72786abaf4bf84
YeasterEgg <l.d.mattiazzi@gmail.com>
2017-02-08 18:07:24 +0100

    wip

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -7,4 +7,21 @@ Spree::OrderMailer.class_eval do
     mail(to: "shop@forst.it", from: "#{@order.email}", subject: subject)
   end
 
+  def order_completed_email(order, resend = false)
+    @host = case Rails.env
+    when "development"
+      "localhost:3000"
+    when "staging"
+      "http://forst-shop.cayennelabs.it"
+    when "production"
+      "https://shop.forst.it"
+    else
+      ""
+    end
+    @order = find_order(order)
+    subject = build_subject(Spree.t('order_mailer.confirm_email.subject'), false)
+
+    mail(to: "shop@forst.it", from: "#{@order.email}", subject: subject)
+  end
+
 end
