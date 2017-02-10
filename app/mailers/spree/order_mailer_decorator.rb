cc03c152fcb46e180ad5d899f0804b68309d0e05
YeasterEgg <l.d.mattiazzi@gmail.com>
2017-02-10 12:38:30 +0100

    added logo

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -5,13 +5,13 @@ Spree::OrderMailer.class_eval do
   def confirm_email(order, resend = false)
     @host = case Rails.env
     when "development"
-      "localhost:3000"
+      "http://localhost:3000"
     when "staging"
       "http://forst-shop.cayennelabs.it"
     when "production"
       "https://shop.forst.it"
     else
-      ""
+      "http://localhost:3000"
     end
     @order = find_order(order)
     subject = build_subject(Spree.t('order_mailer.confirm_email.subject'), false)
