e259c4cf4d70275be771194aa848bcb4049b0437
YeasterEgg <l.d.mattiazzi@gmail.com>
2017-02-21 14:25:59 +0100

    decorate order controller

---
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -0,0 +1,30 @@
+Spree::OrdersController.class_eval do
+
+  before_action :dummy_validate_first_state_order, only: :update
+
+  def update
+    if @order.contents.update_cart(order_params)
+      respond_with(@order) do |format|
+        format.html do
+          if params.key?(:checkout)
+            @order.next if @order.cart?
+            redirect_to checkout_state_path(@order.checkout_steps.first)
+          else
+            redirect_to cart_path
+          end
+        end
+      end
+    else
+      respond_with(@order)
+    end
+  end
+
+  def dummy_validate_first_state_order
+    if(@order.state == "address" && @order.ship_address && !@order.ship_address.valid? && @order.ship_address && !@order.bill_address.valid?)
+      @order.update_attribute(:state, "cart")
+      @order.ship_address.delete
+      @order.bill_address.delete
+    end
+  end
+
+end
